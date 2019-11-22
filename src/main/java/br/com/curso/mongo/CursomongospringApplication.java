a4b1a2e2ee438b9ddfb31fd12953e582520b196c
Alessandro Alves <alves.s.alessandro@gmail.com>
2019-11-22 16:09:46 -0200

    primeiro commit

---
+++ src/main/java/br/com/curso/mongo/CursomongospringApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomongospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomongospringApplication.class, args);
+	}
+
+}
